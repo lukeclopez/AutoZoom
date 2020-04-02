b0308f76a03f6e8e94eae696969baa47eb85ee0b
Luke Lopez <xlucas14@gmail.com>
2020-04-02 00:13:19 -0400

    Don't rely on env vars for id and pw, added timeout to locator

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pyautogui as pag
 import psutil
+import time
 import os
 
 ZOOM_PATH = r"C:\Users\xluca\AppData\Roaming\Zoom\bin\Zoom.exe"
-MEETING_ID = os.getenv("MEETING_ID")
-MEETING_PASSWORD = os.getenv("MEETING_PASSWORD")
 
 
-def main():
+def main(meeting_id, meeting_password):
     open_zoom()
     find_and_click_center("join.png")
     move_mouse_out_of_way()
     find_and_click_center("enter-meeting-id.png")
-    pag.write(MEETING_ID)
+    pag.write(meeting_id)
     find_and_click_center("join-after-meeting-id.png")
     find_and_click_center("enter-meeting-password.png")
-    pag.write(MEETING_PASSWORD)
+    pag.write(meeting_password)
     find_and_click_center("join-meeting-after-password.png")
 
 
@@ -23,12 +22,15 @@ def move_mouse_out_of_way():
     pag.moveTo(100, 200)
 
 
-def find_and_click_center(image):
+def find_and_click_center(image, time_left=5):
     loc = None
-    while loc == None:
+    while loc == None and time_left > 0:
         loc = pag.locateCenterOnScreen("images/" + image, confidence=0.9)
-    x, y = loc
-    pag.click(x=x, y=y)
+        time_left -= 1
+
+    if loc:
+        x, y = loc
+        pag.click(x=x, y=y)
 
 
 def open_zoom():
@@ -36,4 +38,4 @@ def open_zoom():
 
 
 if __name__ == "__main__":
-    main()
+    main(input("ID: "), input("PW: "))
