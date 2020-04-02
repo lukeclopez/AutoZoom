cb19e1571dc245215464004e12e5e05f802a1be6
Luke Lopez <xlucas14@gmail.com>
2020-04-02 00:24:15 -0400

    Window is now maximized on start, esc now closes window

--- gui.py
+++ gui.py
@@ -3,18 +3,12 @@ import tkinter.font as font
 
 from main import main as script
 
-WIDTH = 900
-HEIGHT = 500
-
 
 class Window:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.geometry(f"{WIDTH}x{HEIGHT}")
-        self.window.attributes("-fullscreen", True)
-        self.fullScreenState = False
-        self.window.bind("<F11>", self.toggle_full_screen)
-        self.window.bind("<Escape>", self.quit_full_screen)
+        self.window.state("zoomed")
+        self.window.bind("<Escape>", quit)
 
         large_font = font.Font(size=150)
 
@@ -33,14 +27,6 @@ class Window:
         ).pack()
 
         self.window.mainloop()
-
-    def toggle_full_screen(self, event):
-        self.fullScreenState = not self.fullScreenState
-        self.window.attributes("-fullscreen", self.fullScreenState)
-
-    def quit_full_screen(self, event):
-        self.fullScreenState = False
-        self.window.attributes("-fullscreen", self.fullScreenState)
 
 
 if __name__ == '__main__':
